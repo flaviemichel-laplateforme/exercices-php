5ef2de6bfa71bfd929bbc985a2cb71b036897af3
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-16 10:24:02 +0200

    presentation

---
+++ presentation.php
@@ -0,0 +1,11 @@
+<?php
+// Présentation 
+$prenom = "Flavie";
+echo $prenom . "<br>";
+
+$description = "Je suis en formation DWWM à Toulon.";
+echo $description . "<br>";
+
+$moi = "J'aime le code et la musique.";
+echo $moi;
+?>
